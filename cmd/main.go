1a8b58f46b552faf9598251fcf1d87696cbc9089
Dimitrii Vinogradov <siteddv@gmail.com>
2021-11-29 06:50:12 +0600

    added config

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	todoListBackend "todolistBackend"
 	"todolistBackend/pkg/handler"
@@ -8,7 +9,16 @@ import (
 	"todolistBackend/pkg/service"
 )
 
+const (
+	configPath = "configs"
+	configName = "config"
+)
+
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
@@ -17,4 +27,10 @@ func main() {
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath(configPath)
+	viper.SetConfigName(configName)
+	return viper.ReadInConfig()
 }
