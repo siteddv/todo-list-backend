0202445c07f20afd5e61b53627f27eb9a3db0701
Dimitrii Vinogradov <siteddv@gmail.com>
2021-11-29 06:15:07 +0600

    init dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"log"
 	todoListBackend "todolistBackend"
 	"todolistBackend/pkg/handler"
+	"todolistBackend/pkg/repository"
+	"todolistBackend/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todoListBackend.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
