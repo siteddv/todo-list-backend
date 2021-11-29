440154acdb29f86c492d409ae0b4b4e55c2183bf
Dimitrii Vinogradov <siteddv@gmail.com>
2021-11-30 00:00:31 +0600

    correct down migration

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,9 +1,9 @@
-DROP TABLE users
+DROP TABLE user_lists;
 
-DROP TABLE todo_lists
+DROP TABLE list_items;
 
-DROP TABLE user_lists
+DROP TABLE todo_lists;
 
-DROP TABLE todo_items
+DROP TABLE users;
 
-DROP TABLE list_items
+DROP TABLE todo_items;
