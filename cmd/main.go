507d0d14e39aa01b5075e197262aa8c1add92b5a
Dimitrii Vinogradov <siteddv@gmail.com>
2021-12-01 19:08:11 +0600

    implemented graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	todo "todolistBackend"
 	"todolistBackend/pkg/handler"
 	"todolistBackend/pkg/logging"
@@ -44,8 +47,26 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		logger.Fatalf("error occured while running http server: %s", err.Error())
+	go func() {
+		if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+			logger.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+	logger.Printf("Todoapp started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logger.Printf("Todoapp shutting down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logger.Errorf("error occured on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logger.Errorf("error occured on db connection closing: %s", err.Error())
 	}
 }
 
